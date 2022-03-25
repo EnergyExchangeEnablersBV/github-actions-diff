ea3a515aa9f6423ef9a7ae9481bf4deedc46103d
Jort Koopmans <jort.koopmans@entrnce.com>
2022-03-25 16:13:01 +0100

    Retry.

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ async function run() {
     const newman = require('newman');
     const jsonlint = require('jsonlint');
     const jsonDiff = require('json-diff');
+    const fs = require('fs');
 
     const resultSet = [];
     const theCollection = './' + `${process.env.INPUT_COLLECTION}`;
 
+    fs.readdirSync('./').forEach(file => {
+     console.log(file);
+    });
     // call newman.run to pass `options` object and wait for callback
     newman.run({
         collection: require(theCollection),
